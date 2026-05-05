3667a913d0e73f350979a6584785d4b12ec5cca0
nayhan2 <nayhan2new@gmail.com>
2026-05-05 10:25:29 +0700

    test

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Assesment struct {
+	ID int
+	UserID int
+	Jawaban [5]int
+	Tanggal string
+	SkorTotal int
+	Kategori string
+	Rekomendasi string
+}
+
+// Variable global
+var dataAssesmen []Assesment
+var idCounter int = 1
 
 func main() {
 	fmt.Println("gemas")
